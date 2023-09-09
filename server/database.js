ab8112c5b7889b9bcf0312045af9b87b0c4647e8
karlafehir <fehirkarla@gmail.com>
2023-09-09 14:56:16 +0200

    client & server structure setup

---
+++ server/database.js
@@ -0,0 +1,35 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise()
+
+async function getRecipes(){
+    const [rows] = await pool.query(`
+        SELECT * FROM recipes`);
+    return rows;
+}
+
+async function getRecipe(id){
+    const [rows] = await pool.query(`
+        SELECT * FROM recipes 
+        WHERE id = ?`, [id]);
+    return rows;
+}
+
+async function createRecipe(title, description, instructions, time){
+    const [result] = await pool.query(
+        `INSERT INTO recipes ( title, description, instructions, time )
+         VALUES (?, ?, ?, ?)`, [ title, description, instructions, time ]);
+    return getRecipe(result.insertId);
+}
+
+
+const result = await getRecipes();
+console.log(result);
