9df11369c2b54a769d618e7e42427212fa58e5f7
karlafehir <fehirkarla@gmail.com>
2023-09-10 02:26:26 +0200

    Popular tab design

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
+import { PopularComponent } from './components/popular/popular.component';
 
 const routes: Routes = [
   { 
     path: 'test', 
     component: TestComponent
+  },
+  { 
+    path: 'popular', 
+    component: PopularComponent
   },
 ];
 
--- client/src/app/components/popular/popular.component.ts
+++ client/src/app/components/popular/popular.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-popular',
   templateUrl: './popular.component.html',
   styleUrls: ['./popular.component.scss']
 })
-export class PopularComponent {
+export class PopularComponent implements OnInit {
+
+  constructor(private apiService : ApiServiceService) {}
+
+  readData: any;
+
+  ngOnInit(): void {
+      this.apiService.getAllData().subscribe((res) =>{
+        console.log(res);
+        this.readData = res
+        console.log(this.readData);
+        
+      });
+  }
 
 }
