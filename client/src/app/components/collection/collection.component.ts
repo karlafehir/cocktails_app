f9875abfce61cf622e785d151dc17c81f8f6df8c
karlafehir <fehirkarla@gmail.com>
2023-09-10 22:38:24 +0200

    Collection - cleanup

--- client/src/app/components/collection/collection.component.ts
+++ client/src/app/components/collection/collection.component.ts
@@ -15,27 +15,26 @@ export class CollectionComponent implements OnInit {
   tastes: any;
 
   ngOnInit(): void {
-      this.apiService.getAllData().subscribe((res) =>{
-        console.log(res);
-        this.readData = res
-        console.log(this.readData);
-        // Filter the data by "taste" property where taste is "Sweet"
-        // this.readData = res.filter((cocktail: { taste: string; }) => cocktail.taste === 'Sweet');
-        // console.log(this.readData);
-
-         // Extract unique "taste" values and store them in this.tastes
-        this.tastes = Array.from(new Set(res.map((cocktail: { taste: string }) => cocktail.taste)));
-
-        console.log(this.tastes);
-      });
-      
+    this.getAllData();
   } 
 
-  
+  getAllData(){
+    this.apiService.getAllData().subscribe((res) =>{
+      console.log(res);
+      this.readData = res;
+      this.getCocktailTypes();
+    });
+  }
 
-  getCocktailByType(){
-    this.readData = this.readData.filter((cocktail: { taste: string; }) => cocktail.taste === 'Sweet');
-    console.log(this.readData);
+  getCocktailTypes(){
+     this.tastes = Array.from(new Set(this.readData.map((cocktail: { taste: string }) => cocktail.taste)));
+     console.log(this.tastes);
+  }
+
+  tasteCocktails : any;
+  getCocktailByType(selectedTaste: string){
+    this.tasteCocktails = this.readData.filter((cocktail: { taste: string; }) => cocktail.taste === selectedTaste);
+    console.log(this.tasteCocktails);
   }
 
 }
