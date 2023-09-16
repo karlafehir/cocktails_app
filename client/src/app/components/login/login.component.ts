e997dc61fa28c33a2d5b3c4f47100d2310247ef5
karlafehir <fehirkarla@gmail.com>
2023-09-16 19:39:55 +0200

    login service & cleanup

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,41 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-interface User {
-  email: string;
-  password: string;
-}
-
-interface LoginResponse {
-  userId: number;
-  token: string;
-}
-
+import { User } from 'src/app/interfaces/User';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
   user: User = {
     email: '',
     password: ''
   };
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private loginService: LoginService) {}
 
   onSubmit() {
-    this.http.post<LoginResponse>('http://localhost:8080/login', this.user).subscribe(
+    this.loginService.login(this.user).subscribe(
       (response) => {
-        const { userId, token } = response;
-        localStorage.setItem('token', token);
-        this.router.navigate(['/collection']);
+        this.loginService.handleLoginResponse(response);
       },
       (err) => {
-        console.log('Error logging in', err);
-      } 
+        this.loginService.handleError(err);
+      }
     );
   }
 }
---
+++ client/src/app/services/login.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/User';
+import { LoginResponse } from '../interfaces/LoginResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(user: User): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>('http://localhost:8080/login', user);
+  }
+
+  handleLoginResponse(response: LoginResponse): void {
+    const { userId, token } = response;
+    localStorage.setItem('token', token);
+    this.router.navigate(['/collection']);
+  }
+
+  handleError(err: any): void {
+    console.error('Error logging in', err);
+  }
+}
